647ddb53fec68ebeb7801316e749e708dacbde16
Amanat Khan Shakil <amanatkhanshakil223@gmail.com>
2024-09-20 23:26:16 +0600

    added pertially the add money event work on progress

--- js/home.js
+++ js/home.js
@@ -1,1 +1,8 @@
-console.log("home");
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const addMoneyInput = document.getElementById("input-add-money").value;
+    const pinNumber = document.getElementById("input-pin-number").value;
+  });
