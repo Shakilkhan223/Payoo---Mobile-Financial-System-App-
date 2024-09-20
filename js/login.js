50c97ee6af458e43fa4d5c10b70e6ec2b720449d
Amanat Khan Shakil <amanatkhanshakil223@gmail.com>
2024-09-20 22:55:56 +0600

    Added Login form and Payoo app home page

--- js/login.js
+++ js/login.js
@@ -4,8 +4,15 @@ document
   .getElementById("button-login")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("login button clicked");
 
     const phoneNumber = document.getElementById("phone-number").value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById("pin-number").value;
+    console.log(phoneNumber, pinNumber);
+
+    if (phoneNumber === "01786212504" && pinNumber === "1234") {
+      console.log("You are logged in");
+      window.location.href = "/home.html";
+    } else {
+      alert("Invalid phone number or pin number");
+    }
   });
